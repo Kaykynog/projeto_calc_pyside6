6150893805eebd5181a01a40a749bd024bb64693
Kayky Nogueira <kayky-silva20@hotmail.com>
2023-05-15 21:05:21 -0300

    15-05-2023
    Até o momento criei e configurei meu display.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import sys
 
+from display import Display
 from main_window import MainWindow
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
+    # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
 
-    label1 = QLabel('nada com nada')
-    label1.setStyleSheet('font-size: 150px;')
-    window.v_layout.addWidget(label1)
-    window.adjustFixedSize()
+    # Define o ícone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
 
+    # Display
+    display = Display()
+    window.addToVLayout(display)
+ 
+    # Executa tudo
+    window.adjustFixedSize()
     window.show()
     app.exec()

670267b46bcc80539d1db88388a8bdcb4fa1d19a
Kayky Nogueira <kayky-silva20@hotmail.com>
2023-05-16 11:56:18 -0300

    16-05-2023
    Atualizações... defini o tema que irei utilizar.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 
 from display import Display
+from info import Info
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
+from style import setupTheme
 from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
@@ -15,6 +17,10 @@ if __name__ == '__main__':
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # Info
+    info = Info('^2.0 ^ 10.0 = 1024')
+    window.addToVLayout(info)
 
     # Display
     display = Display()
